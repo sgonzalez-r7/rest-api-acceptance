8aa5d593df4176f4d5c22c32ad41fb0e1e9371e1
Sonny Gonzalez <sonny_gonzalez@rapid7.com>
2015-01-11 16:33:01 -0600

    load token from key.json

--- features/support/rest_api_helpers.rb
+++ features/support/rest_api_helpers.rb
@@ -1,18 +1,25 @@
-module RestApiHelpers
-  def base_uri
-    'http://0.0.0.0:5000/rest_api/v2'
-  end
-
-  def http_with_headers
-    HTTP.with(accept: 'application/json',
-              'Token' => '75e3db94166b7d1f60994a41b573e6ad')
-  end
-end
-World(RestApiHelpers)
-
 module FileHelpers
   def data_dir
     File.dirname(__FILE__) + '/../data/'
   end
 end
 World(FileHelpers)
+
+
+module RestApiHelpers
+  include FileHelpers
+
+  def base_uri
+    'http://0.0.0.0:5000/rest_api/v2'
+  end
+
+  def token
+    json_key = JSON.parse File.read(data_dir + 'key.json')
+    json_key['token']
+  end
+
+  def http_with_headers
+    HTTP.with(accept: 'application/json', 'Token' => token)
+  end
+end
+World(RestApiHelpers)
