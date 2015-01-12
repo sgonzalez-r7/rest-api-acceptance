4057dd833048e71c4107345b72d17928cb397dcf
Sonny Gonzalez <sonny_gonzalez@rapid7.com>
2015-01-12 12:45:41 -0600

    white

--- features/step_definitions/rest_api_steps.rb
+++ features/step_definitions/rest_api_steps.rb
@@ -8,7 +8,7 @@ end
 
 Given(/^the database has (.*?)$/) do |resource|
   @json_database = JSON.parse(File.read(data_dir + resource + '.json'))
-  @json_database.sort! { |x,y| x['id'] <=> y['id']}  
+  @json_database.sort! { |x,y| x['id'] <=> y['id'] }
 end
 
 And(/the client makes a GET request/) do
@@ -28,7 +28,7 @@ And(/the response is JSON/) do
 end
 
 And(/it returns all (.*?) in the database/) do |resource|
-  @json_response.sort! {|x,y| x['id'] <=> y['id']}
+  @json_response.sort! { |x,y| x['id'] <=> y['id'] }
   expect(@json_response).to eql @json_database
 end
 
