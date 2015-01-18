79798768c43dbe816afe2c410bf3c3eceffb92d3
Sonny Gonzalez <sonny_gonzalez@rapid7.com>
2015-01-17 20:01:01 -0600

    add find and note

--- lib/rest_api_validation/tester.rb
+++ lib/rest_api_validation/tester.rb
@@ -4,12 +4,23 @@ class  Tester
 
   def initialize(database:)
     @database = database
+    @notes    = {}
   end
 
   def find_and_note_id_of_parent_with_children(parent, children)
     child_name = children.singularize
     child      = database.fetch_data_for(child_name).first
-    child[parent + '_id']
+    param      = parent + '_id'
+    parent_id  = child[param]
+    note(param.to_sym => parent_id)
+    parent_id
+  end
+
+  private
+
+  def note(params)
+    k, v = params.shift
+    @notes[k] = v
   end
 end
 end
--- spec/rest_api_validation/tester_spec.rb
+++ spec/rest_api_validation/tester_spec.rb
@@ -3,10 +3,17 @@ require 'spec_helper'
 module RestApiValidation
 describe Tester do
   describe 'find_and_note_parent_with_children' do
-    it 'finds a parent with children' do
+    it 'finds an id of a parent with children' do
       tester    = Tester.new database: Database
       parent_id = tester.find_and_note_id_of_parent_with_children('host', 'sessions')
       expect(parent_id).to eql 116
+    end
+
+    it 'notes the parent id' do
+      tester    = Tester.new database: Database
+      expect(tester).to receive(:note).with(host_id: 116)
+      parent_id = tester.find_and_note_id_of_parent_with_children('host', 'sessions')
+
     end
   end
 end
