f49236051fcc75838fe9cec93d222e20f49127c2
Sonny Gonzalez <sonny_gonzalez@rapid7.com>
2015-01-24 18:33:21 -0600

    make database spec simpler

--- spec/rest_api_validation/database_spec.rb
+++ spec/rest_api_validation/database_spec.rb
@@ -15,39 +15,7 @@ describe Database do
     it 'fetches data with an attribute' do
       database = Database.new data_dir: test_data_dir
       host     = database.fetch_data_for('hosts', id: 2).first
-      data     = JSON.parse <<-eod
-        {
-          "address": "192.168.0.5",
-          "arch": null,
-          "comm": null,
-          "comments": null,
-          "created_at": "2015-01-20T18:47:19Z",
-          "cred_count": 0,
-          "detected_arch": null,
-          "exploit_attempt_count": 0,
-          "history_count": 0,
-          "host_detail_count": 0,
-          "id": 2,
-          "info": null,
-          "mac": null,
-          "name": "mdm_host_2",
-          "nexpose_data_asset_id": null,
-          "note_count": 0,
-          "os_flavor": null,
-          "os_lang": null,
-          "os_name": null,
-          "os_sp": null,
-          "purpose": null,
-          "scope": null,
-          "service_count": 0,
-          "state": null,
-          "updated_at": "2015-01-20T18:47:19Z",
-          "virtual_host": null,
-          "vuln_count": 0,
-          "workspace_id": 2
-        }
-      eod
-      expect(host).to eql data
+      expect(host['id']).to eql 2
     end
   end
 
