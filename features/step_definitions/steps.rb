3cd18de62ee457f35b5cf212071cb237dbc5f7e7
Sonny Gonzalez <sonny_gonzalez@rapid7.com>
2015-01-25 16:15:13 -0600

    the parent has children

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,20 +1,20 @@
 Given(/^a (\S+) that has a (\S+)$/) do |parent_name, child_name|
   parent = database.fetch_a(parent_name.to_sym)
   child  = database.fetch_a(child_name.to_sym,
-                    "#{parent_name}_id".to_sym => parent.id)
+                            "#{parent_name}_id".to_sym => parent.id)
 
-  params["#{parent}_id".to_sym] = parent.id
-  params["#{child}_id".to_sym]  = child.id
+  params["#{parent_name}_id".to_sym] = parent.id
+  params["#{child_name}_id".to_sym]  = child.id
 
   expect(child["#{parent_name}_id"]).to eql parent.id
 end
 
-Given(/^the host has sessions$/) do |parent_name, child_name|
+Given(/^the (\S+) has (\S+)$/) do |parent_name, child_name|
   parent   = database.fetch_a(parent_name.to_sym,
-                      id: params["#{parent_name}_id".to_sym])
+                              id: params["#{parent_name}_id".to_sym])
 
   children = database.fetch_data_for(child_name.to_sym,
-                      "#{parent}_id".to_sym => parent.id)
+                                     "#{parent_name}_id".to_sym => parent.id)
 
   expect(children.count).to be > 0
 end
