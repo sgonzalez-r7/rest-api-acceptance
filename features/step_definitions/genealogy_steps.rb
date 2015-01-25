eaec940314f133d7081bdaec6837b9574eaa2f50
Sonny Gonzalez <sonny_gonzalez@rapid7.com>
2015-01-25 16:57:05 -0600

    white and comments

--- features/step_definitions/genealogy_steps.rb
+++ features/step_definitions/genealogy_steps.rb
@@ -1,5 +1,9 @@
+#
+# a parent that has a child
+#
 Given(/^a (\S+) that has a (\S+)$/) do |parent_name, child_name|
   parent = database.fetch_a(parent_name.to_sym)
+
   child  = database.fetch_a(child_name.to_sym,
                             "#{parent_name}_id".to_sym =>
                               parent.id)
@@ -10,7 +14,9 @@ Given(/^a (\S+) that has a (\S+)$/) do |parent_name, child_name|
   expect(child["#{parent_name}_id"]).to eql parent.id
 end
 
-
+#
+# the parent has children
+#
 Given(/^the (\S+) has (\S+)$/) do |parent_name, child_name|
   parent   = database.fetch_a(parent_name.to_sym,
                               id: params["#{parent_name}_id".to_sym])
@@ -18,23 +24,30 @@ Given(/^the (\S+) has (\S+)$/) do |parent_name, child_name|
   children = database.fetch_data_for(child_name.to_sym,
                                      "#{parent_name}_id".to_sym =>
                                        parent.id)
-
   expect(children.count).to be > 0
 end
 
+#
+# a resource that exists
+#
 Given(/^a (\S+) that exists$/) do |resource_name|
   resource = database.fetch_a(resource_name.to_sym)
   params[resource_name.to_sym] = resource.id
 end
 
-
+#
+# a resource that does NOT exist
+#
 Given(/^a (\S+) that does (NOT) exist$/) do |resource, arg|
   params["#{resource}_id".to_sym] = 999
 end
 
-
+#
+# a parent that has children
+#
 Given(/^a (\S+) that has (\S+)$/) do |parent_name, child_name|
   parent   = database.fetch_a("#{parent_name}".to_sym)
+
   children = database.fetch_data_for("#{child_name}".to_sym,
                                      "#{parent_name}_id".to_sym =>
                                        parent.id)
--- features/step_definitions/rest_api_steps.rb
+++ features/step_definitions/rest_api_steps.rb
@@ -1,13 +1,24 @@
+#
+# the client makes a GET#index request for resource
+#
 When(/^the client makes a (GET\#index) request for (\S+)$/) do |arg, resource_name|
   client.make_get_request :index, resource_name.to_sym, params
 end
 
+#
+# the status code is AN_INTEGER
+#
 Then(/^the status code is (#{AN_INTEGER})$/) do |code|
   expect(client.last_response.code).to eql code
 end
 
+
+#
+# it returns all children for the parent
+#
 Then(/^it returns all (\S+) for the (\S+)$/) do |child_name, parent_name|
   ids      = json_to_ids(client.last_response.to_s)
+
   data_ids = database.fetch_ids_for(child_name.to_sym,
                                     "#{parent_name}_id".to_sym =>
                                       params["#{parent_name}_id".to_sym])
