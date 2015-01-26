00f6335f44800cbde5182ad1d3402653be77b0fd
Sonny Gonzalez <sonny_gonzalez@rapid7.com>
2015-01-26 09:39:11 -0600

    white

--- features/step_definitions/edge_case_steps.rb
+++ features/step_definitions/edge_case_steps.rb
@@ -9,8 +9,7 @@ end
 # a child that does NOT belong to a parent
 #
 Given(/^a (\S+) that does (NOT) belong to a (\S+)$/) do |child_name, arg, parent_name|
-  parent   = database.fetch_a("#{parent_name}".to_sym)
-
+  parent       = database.fetch_a("#{parent_name}".to_sym)
   child_other  = database.fetch_an_other("#{child_name}".to_sym)
 
   params["#{parent_name}_id".to_sym] = parent.id
